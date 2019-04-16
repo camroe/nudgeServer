e74c005e701f66d3f813bd90ade145ef6f7ecdec
Cameron Roe <croe10@ford.com>
2019-04-16 16:00:47 -0400

    Inital services with Retrofit working. GET only.

--- src/main/java/com/ford/cvpp/nudgeserver/TomcatController.java
+++ src/main/java/com/ford/cvpp/nudgeserver/TomcatController.java
@@ -11,29 +11,53 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class TomcatController {
 
-  @GetMapping("/nudge")
-  public String getNudge() {
-    return "You've been nudged!";
-  }
+    @GetMapping("/nudge")
+    public String getNudge() {
+        String returnString =
+                new StringBuilder()
+                        .append("{")
+                        .append("\"")
+                        .append("message")
+                        .append("\"")
+                        .append(":")
+                        .append("\"")
+                        .append("You've been nudged!")
+                        .append("\"")
+                        .append("}")
+                        .toString();
+        System.out.println(returnString);
+        return returnString;
+    }
 
-  @PostMapping(path = "/nudge", consumes = MimeTypes.Application.JSON)
-  public String postNudge(@RequestBody NudgeCommand nudgeCommand) {
-    NudgeCommand sampleNudge = new NudgeCommand("sampleNudge","sampleVehID","2 feet","forward");
-    ObjectMapper mapper = new ObjectMapper();
-    String result = "Fail\n";
-    try {
-      String sampleJSON = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(sampleNudge);
-      System.out.println("SampleJSON: \n"+sampleJSON);
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
+    @PostMapping(path = "/nudge", consumes = MimeTypes.Application.JSON)
+    public String postNudge(@RequestBody NudgeCommand nudgeCommand) throws JsonProcessingException {
+        NudgeCommand sampleNudge = new NudgeCommand("sampleNudge", "sampleVehID", "2 feet", "forward");
+        ObjectMapper mapper = new ObjectMapper();
+        String result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(sampleNudge);
+        try {
+            String sampleJSON = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(sampleNudge);
+            System.out.println("SampleJSON: \n" + sampleJSON);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        try {
+            String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(nudgeCommand);
+            System.out.println("Actual JSON = " + json);
+
+//      result = new StringBuilder()
+//              .append("{\"message\":\"Success: Vehicle ")
+//              .append(nudgeCommand.getVehicleID())
+//              .append(" has been nudged ")
+//              .append(nudgeCommand.getDistance())
+//              .append(" ")
+//              .append(nudgeCommand.getDirection())
+//              .append("\n").append(json)
+//              .append("\"}")
+//              .toString();
+            result = json;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
-    try {
-      String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(nudgeCommand);
-      System.out.println("Actual JSON = " + json);
-      result = "Success: Vehicle " + nudgeCommand.getVehicleID() + " has been nudged " + nudgeCommand.getDistance() + " " + nudgeCommand.getDirection() + "\n"+json;
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-    }
-    return result;
-  }
 }
